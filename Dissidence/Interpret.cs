e8ea35b438ef5d366952ad1afdbd11b15bebf275
somebody1234 <ehern.lee@gmail.com>
2020-06-29 09:14:32 +0000

    finish interpret.cs

--- Dissidence/Interpret.cs
+++ Dissidence/Interpret.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Metatron.Dissidence.Node;
 
 namespace Metatron.Dissidence {
@@ -18,11 +19,42 @@ namespace Metatron.Dissidence {
                     Variable node => scope[node.Name],
                     // TODO: make sure that body returns unit when typechecking
                     If node => ((bool) Evaluate(node.Condition, scope)) ? Evaluate(node.Body, scope) : new Prelude.Unit(),
-                    // Call node => replacement == node.Function ? node with { Function = replacement } : node with { Arguments = node.Arguments.Select(item => item == target ? replacement : item).ToList() },
-                    // While node => replacement == node.Condition ? node with { Condition = replacement } : node with { Body = replacement },
-                    // Match node => replacement == node.Value ? node with { Value = replacement } : node with { Arms = node.Arms.Select(item => item == target ? replacement : item).ToList() },
-                    // Mapping node => replacement == node.Value ? node with { Value = replacement } : node with { Body = replacement },
-                    // Block node => node with { Statements = node.Statements.Select(item => item == target ? replacement : item).ToList() },
+                    Call node => ((Func<object>) (() => {
+                        var function = Evaluate(node.Function, scope);
+                        if (!(function is Delegate)) {
+                            throw new ArgumentException("Not a function; cannot be called");
+                        }
+                        return ((Delegate) function).DynamicInvoke(node.Arguments.Select(argument => Evaluate(argument, scope)).ToArray());
+                    }))(),
+                    While node => ((Func<object>) (() => {
+                        // TODO: this is inconsistent (cannot be typechecked) if body may run 0 times.
+                        var result = (object) new Prelude.Unit();
+                        while ((bool) Evaluate(node.Condition, scope)) {
+                            result = Evaluate(node.Body, scope);
+                        }
+                        return result;
+                    }))(),
+                    Match node => ((Func<object>) (() => {
+                        var target2 = Evaluate(node.Value, scope);
+                        foreach (var arm in node.Arms) {
+                            if (!(arm is Mapping)) {
+                                throw new ArgumentException("Expected mapping node in match arm");
+                            }
+                            if (target2 == Evaluate(((Mapping) arm).Value, scope)) {
+                                return Evaluate(((Mapping) arm).Body, scope);
+                            }
+                        }
+                        throw new ArgumentException("This match is not exhaustive");
+                    }))(),
+                    Mapping node => throw new ArgumentException("Mapping node cannot be used outside of match arm"),
+                    Block node => ((Func<object>) (() => {
+                        var result = (object) new Prelude.Unit();
+                        var scope2 = new Scope(scope);
+                        foreach (var statement in node.Statements) {
+                            result = Evaluate(statement, scope2);
+                        }
+                        return result;
+                    }))(),
                     _ => throw new ArgumentException("Unknown node type found when interpreting AST"),
             };
         }
